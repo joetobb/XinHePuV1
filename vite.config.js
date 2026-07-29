f8713fc1c981e167a5861dc20aec7eedbd9eb6ed
Joemi <1843510043@qq.com>
2026-07-29 12:02:24 +0800

    Change base path to '/XinHePuV1/'
    Updated base path in Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // 👇 必须和你的仓库名完全一致，带斜杠
-  base: '/HNanQilouTestV1.2/',
+  base: '/XinHePuV1/',
   resolve: {
     alias: {
       cesium: 'cesium/Build/Cesium'
